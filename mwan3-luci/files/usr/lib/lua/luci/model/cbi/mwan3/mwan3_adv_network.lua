083e833c28d9e63dcf0a6acb7c9aac9f48fa7acd
Aedan Reindeer <chipdankly@gmail.com>
2013-06-28 03:32:49 -0700

    updated instructions on controlling router-generated traffic

--- mwan3-luci/files/usr/lib/lua/luci/model/cbi/mwan3/mwan3_adv_network.lua
+++ mwan3-luci/files/usr/lib/lua/luci/model/cbi/mwan3/mwan3_adv_network.lua
@@ -10,15 +10,19 @@ m = SimpleForm("networkconf", nil)
 
 f = m:section(SimpleSection, nil,
 	translate("<br />This section allows you to modify the contents of /etc/config/network<br /><br />" ..
-	"To allow MWAN3 to control router-generated traffic you must:<br />" ..
+	"To allow MWAN3 control of router-generated traffic you must complete the following three steps:<br /><br />" ..
 	"1. Create a loopback alias config section in /etc/config/network<br /><br />" ..
 	"config alias<br />" ..
 	"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;option interface &#39;loopback&#39;<br />" ..
 	"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;option proto &#39;static&#39;<br />" ..
-	"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;option ipaddr &#39;10.10.10.1&#39;<br />" ..
+	"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;option ipaddr &#39;192.168.100.1&#39;<br />" ..
 	"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;option netmask &#39;255.255.255.255&#39;<br /><br />" ..
 	"The ipaddr option must be an IP address from an unused network in these ranges: 10.x.x.x, 172.16.x.x-172.31.255.255, 192.168.x.x<br /><br />" ..
-	"2. Create your desired MWAN3 rules for router-generated traffic with the source address set to the loopback alias IP address<br /><br />"))
+	"2. Add a default route command to the startup commands tab<br /><br />" ..
+	"ip route add default via 127.0.0.1 dev lo src 192.168.100.1 metric 5<br /><br />" ..
+	"Configure the source using the IP address from the loopback alias<br />" ..
+	"The metric must be lower than any interface metric in /etc/config/network<br /><br />" ..
+	"3. Create your desired MWAN3 rules for router-generated traffic with the source address set to the loopback alias IP address<br /><br />"))
 
 t = f:option(TextValue, "lines")
 	t.rmempty = true
