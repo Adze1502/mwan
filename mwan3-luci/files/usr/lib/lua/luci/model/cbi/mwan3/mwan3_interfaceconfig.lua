e48c4873ce2303d606ed55d40025868abce5d438
Aedan ARFETT Renner <chipdankly@gmail.com>
2014-02-12 14:31:48 -0800

    E-mail alerts now show the WAN interfaces' IP addresses. Credits to Thomymaster for the idea.

--- mwan3-luci/files/usr/lib/lua/luci/model/cbi/mwan3/mwan3_interfaceconfig.lua
+++ mwan3-luci/files/usr/lib/lua/luci/model/cbi/mwan3/mwan3_interfaceconfig.lua
@@ -106,7 +106,7 @@ interval = mwan_interface:option(ListValue, "interval", translate("Ping interval
 	interval:value("600", translate("10 minutes"))
 	interval:value("900", translate("15 minutes"))
 	interval:value("1800", translate("30 minutes"))
-	interval:value("3600", translate("60 minutes"))
+	interval:value("3600", translate("1 hour"))
 
 down = mwan_interface:option(ListValue, "down", translate("Interface down"),
 	translate("Interface will be deemed down after this many failed ping tests"))
